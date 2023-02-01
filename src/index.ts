7cd2ac827a8bb9c3f37d903f73cff7f3195df732
Robinson Ody <robinsonody@Robinsons-MacBook-Pro.local>
2023-02-01 09:26:59 +0700

    Fixing messed up variable name

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 import getPageArray from './get-page-array';
-import thousand_separator from './thousand-separator';
+import thousandSeparator from './thousand-separator';
 import plainNumber from './plain-num';
 import hashMD5 from './hash-md5';
 import randomChalk from './random-chalk';
 
-export {
-  getPageArray as get_page_array,
-  thousand_separator,
-  plainNumber as plain_number,
-  hashMD5 as hash_md5,
-  randomChalk as random_chalk,
-};
+export { getPageArray, thousandSeparator, plainNumber, hashMD5, randomChalk };
