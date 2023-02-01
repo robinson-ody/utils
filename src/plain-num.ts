5ddaa811baa316b976033e811d145d984fd8dfe4
Robinson Ody <robinsonody@Robinsons-MacBook-Pro.local>
2023-02-01 09:21:55 +0700

    Fix shadowed variable

--- src/plain-num.ts
+++ src/plain-num.ts
@@ -2,8 +2,8 @@
  * Removes thousand separators and omits decimal places from a number string
  */
 const plainNumber = (inputNumber: string): number => {
-  const plainNumber = inputNumber.replace(/[^0-9]/g, '');
-  return Number(plainNumber);
+  const outputNumber = inputNumber.replace(/[^0-9]/g, '');
+  return Number(outputNumber);
 };
 
 export default plainNumber;
