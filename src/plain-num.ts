68e8137511eeed23c2e54555b4a6126386d114fc
Robinson Ody <robinsonody@Robinsons-MacBook-Pro.local>
2023-02-01 09:21:28 +0700

    Fix variable name

--- src/plain-num.ts
+++ src/plain-num.ts
@@ -2,8 +2,8 @@
  * Removes thousand separators and omits decimal places from a number string
  */
 const plainNumber = (inputNumber: string): number => {
-  const number = inputNumber.replace(/[^0-9]/g, '');
-  return Number(number);
+  const plainNumber = inputNumber.replace(/[^0-9]/g, '');
+  return Number(plainNumber);
 };
 
 export default plainNumber;
