50cc1a0157e87f9a35baca849592ef45f330d6ff
Robinson Ody <robinsonody@Robinsons-MacBook-Pro.local>
2023-03-08 17:16:30 +0700

    Removed hash-md5 import from index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import getPageArray from './get-page-array';
 import thousandSeparator from './thousand-separator';
 import plainNumber from './plain-num';
-import hashMD5 from './hash-md5';
 import randomChalk from './random-chalk';
 
-export { getPageArray, thousandSeparator, plainNumber, hashMD5, randomChalk };
+export { getPageArray, thousandSeparator, plainNumber, randomChalk };
