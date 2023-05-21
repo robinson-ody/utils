4e062aa5662c3d399b693e0ff466bc4f42a58c82
Robinson Ody <robinsonody@gmail.com>
2023-05-21 16:21:01 +0700

    Aligning asterisk in jsdoc

--- src/random-number.ts
+++ src/random-number.ts
@@ -4,10 +4,10 @@ interface RandomNumberParams {
 }
 
 /**
-  * The min and max is inclusive.
-  * Meaning that if you input min: 1 and max: 5,
-  * the result is one of 1, 2, 3, 4, 5
-  */
+ * The min and max is inclusive.
+ * Meaning that if you input min: 1 and max: 5,
+ * the result is one of 1, 2, 3, 4, 5
+ */
 const getRandomNumber = ({ min, max }: RandomNumberParams) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
