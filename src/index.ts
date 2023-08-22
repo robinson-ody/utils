cfe51478bcf0829942ba87431faae587ecaa2088
Robinson Ody <robinsonody@gmail.com>
2023-08-22 22:39:40 +0700

    removed deep log because of unknown problem

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import getPageArray from './get-page-array';
 import thousandSeparator from './thousand-separator';
 import plainNumber from './plain-num';
 import randomChalk from './random-chalk';
-import deepLog from './deep-log';
 import getRandomNumber from './random-number';
 
-export { getPageArray, thousandSeparator, plainNumber, randomChalk, deepLog, getRandomNumber };
+export { getPageArray, thousandSeparator, plainNumber, randomChalk, getRandomNumber };
