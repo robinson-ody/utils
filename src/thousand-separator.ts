993343ef013414a96ff9b559522447cf31be689b
Robinson Ody <robinsonody@gmail.com>
2023-08-23 18:53:07 +0700

    thousand separator added support for negative num

--- src/thousand-separator.ts
+++ src/thousand-separator.ts
@@ -20,6 +20,15 @@ const thousandSeparator = ({
 }: ThousandSeparatorParams): string | number => {
   if (!numbers || isNaN(Number(numbers))) return empty_placeholder;
   let result = '';
+
+  if (typeof numbers === 'number') numbers = numbers.toString();
+  if (numbers.length < 4) return numbers;
+
+  if (numbers.substring(0, 1) === '-') {
+    numbers = numbers.substring(1);
+    result = '-';
+  }
+
   const numbersArr = numbers.toString().split('').reverse();
 
   for (const i in numbersArr) {
