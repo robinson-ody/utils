71cc5f78958e0f8ec321eb26bd23bb141f478bd7
Robinson Ody <robinsonody@gmail.com>
2024-01-09 22:20:46 +0700

    import to index and re-export

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,15 @@ import thousandSeparator from './thousand-separator';
 import plainNumber from './plain-num';
 import randomChalk from './random-chalk';
 import getRandomNumber from './random-number';
+import randomLightChalk from './random-light-chalk';
+import randomDarkChalk from './random-dark-chalk';
 
-export { getPageArray, thousandSeparator, plainNumber, randomChalk, getRandomNumber };
+export {
+  getPageArray,
+  thousandSeparator,
+  plainNumber,
+  randomChalk,
+  getRandomNumber,
+  randomLightChalk,
+  randomDarkChalk,
+};
