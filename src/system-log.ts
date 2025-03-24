88e0d395c5bf7b13032264540ec104afcb49ca29
Robinson Ody <robinsonody@gmail.com>
2025-03-24 11:44:04 +0700

    Refactor system_log function to include optional user parameter and improve date formatting; remove unnecessary .DS_Store file

--- src/system-log.ts
+++ src/system-log.ts
@@ -1,15 +1,20 @@
 import { DateTime } from 'luxon';
 import { random_chalk } from './random-chalk';
 
-const get_date = () => DateTime.now().toFormat('yyyy-MM-dd HH:mm:ss');
+const get_date = () => {
+  return DateTime.now().toFormat('yyyy-MM-dd HH:mm:ss');
+};
 
 interface SystemLogParams {
   message: string;
   log_key: string;
+  user?: string;
 }
 
 const system_log = (params: SystemLogParams) => {
-  random_chalk(`${get_date()} | ${params.log_key} | ${params.message}`);
+  const { message, log_key, user } = params;
+  const user_str = user ? `${user}` : 'system';
+  random_chalk(`${get_date()} | ${user_str} | ${log_key} | ${message}`);
 };
 
 export { system_log };
